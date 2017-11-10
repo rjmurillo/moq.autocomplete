50275fcb695126c94cc68989249fa978eb88f835
Litee <lipatkin@gmail.com>
2017-11-10 09:37:11 +0300

    Set up an empty VS solution

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Test.Mocks.Edit
-Visual Studio extension and Roslyn analyzer that helps to write unit tests using Moq and NSubstitute mocking libraries
+Visual Studio extension and Roslyn analyzer that helps to write unit tests using Moq and NSubstitute mocking libraries. Migration of AgentZorge Resharper plugin to pure Roslyn.
---
+++ Test.Mocks.Edit/Test.Mocks.Edit/DiagnosticAnalyzer.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Test.Mocks.Edit
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class TestMocksEditAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "TestMocksEdit";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+        }
+
+    }
+}
