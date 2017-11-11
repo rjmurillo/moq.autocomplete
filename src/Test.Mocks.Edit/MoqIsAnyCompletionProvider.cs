b5ca140905c38135f51dd44d8de5ff050fd29945
Litee <lipatkin@gmail.com>
2017-11-11 09:18:46 +0300

    Changed folders structure

---
+++ src/Test.Mocks.Edit/MoqIsAnyCompletionProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Completion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Mocks.Edit
+{
+    [ExportCompletionProvider(nameof(MoqIsAnyCompletionProvider), LanguageNames.CSharp)]
+    public class MoqIsAnyCompletionProvider : CompletionProvider
+    {
+    }
+}
